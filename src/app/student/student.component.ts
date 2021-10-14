6fb2e57b586d069d0553a71ad0bab3bc7cd1b952
MarengoSimone <s.marengo.1338@vallauri.edu>
2021-10-14 10:58:20 +0200

    Directive *ngif and else introduced

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -9,18 +9,18 @@ export class StudentComponent implements OnInit {
 
   // creazione lista studenti
   studentList = [
-    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M' },
-    { name: 'Luca Pelissero', hobby: 'Arrampicata', gender: 'M' },
-    { name: 'Bianca Teleman', hobby: 'Volley', gender: 'F' },
-    { name: 'Paolo Giordano', hobby: 'Volley', gender: 'M' },
-    { name: 'Michele Ghisolfi', hobby: 'Judo', gender: 'M' },
-    { name: 'Veronico Galvagno', hobby: 'Paddle', gender: 'F' },
-    { name: 'Chiara Pelissero', hobby: 'Paddle', gender: 'F' },
-    { name: 'Federico Mameli', hobby: 'Danza', gender: 'M' },
-    { name: 'Lorenzo Cravero', hobby: 'Calcio', gender: 'M' },
-    { name: 'Marta Baudracco', hobby: 'Volley', gender: 'F' },
-    { name: 'Edoardo Ababei', hobby: 'Palestra', gender: 'M' },
-    { name: 'Ivan Anjelovski', hobby: 'Palestra', gender: 'M' }
+    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M',isPro:false },
+    { name: 'Luca Pelissero', hobby: 'Arrampicata', gender: 'M',isPro:true },
+    { name: 'Bianca Teleman', hobby: 'Volley', gender: 'F',isPro:false },
+    { name: 'Chiara Giordano', hobby: 'Volley', gender: 'F',isPro:true },
+    { name: 'Michele Ghisolfi', hobby: 'Judo', gender: 'M',isPro:false },
+    { name: 'Simone Marengo', hobby: 'Paddle', gender: 'M',isPro:true },
+    { name: 'Paolo Vietti', hobby: 'Paddle', gender: 'M',isPro:false },
+    { name: 'Federico Mameli', hobby: 'Danza', gender: 'M',isPro:false },
+    { name: 'Lorenzo Cravero', hobby: 'Calcio', gender: 'M',isPro:true },
+    { name: 'Marta Baudracco', hobby: 'Volley', gender: 'F',isPro:true },
+    { name: 'Edoardo Ababei', hobby: 'Palestra', gender: 'M',isPro:false },
+    { name: 'Ivan Anjelovski', hobby: 'Palestra', gender: 'M',isPro:true }
   ]
 
   student: any;
