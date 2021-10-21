a50c9fda586b9d9834f467cc067a27c733f593f2
MarengoSimone <s.marengo.1338@vallauri.edu>
2021-10-21 10:46:37 +0200

    Started to manage add Student and update component.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,19 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild("txtName") txtName !: ElementRef;
+
   title = '5 INF B 2021/22';
-  studentList = [
-    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M',isPro:false },
-    { name: 'Luca Pelissero', hobby: 'Arrampicata', gender: 'M',isPro:false },
-    { name: 'Bianca Teleman', hobby: 'Volley', gender: 'F',isPro:false },
-    { name: 'Chiara Giordano', hobby: 'Volley', gender: 'F',isPro:false },
-    { name: 'Michele Ghisolfi', hobby: 'Judo', gender: 'M',isPro:false },
-    { name: 'Simone Marengo', hobby: 'Paddle', gender: 'M',isPro:false },
-    { name: 'Paolo Vietti', hobby: 'Paddle', gender: 'M',isPro:false },
-    { name: 'Federico Mameli', hobby: 'Danza', gender: 'M',isPro:false },
-    { name: 'Lorenzo Cravero', hobby: 'Calcio', gender: 'M',isPro:false },
-    { name: 'Marta Baudracco', hobby: 'Volley', gender: 'F',isPro:false },
-    { name: 'Edoardo Ababei', hobby: 'Palestra', gender: 'M',isPro:false },
-    { name: 'Ivan Anjelovski', hobby: 'Palestra', gender: 'M',isPro:false }
-  ]
+  // studentList = [
+  //   { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M',isPro:false },
+  //   { name: 'Luca Pelissero', hobby: 'Arrampicata', gender: 'M',isPro:false },
+  //   { name: 'Bianca Teleman', hobby: 'Volley', gender: 'F',isPro:false },
+  //   { name: 'Chiara Giordano', hobby: 'Volley', gender: 'F',isPro:false },
+  //   { name: 'Michele Ghisolfi', hobby: 'Judo', gender: 'M',isPro:false },
+  //   { name: 'Simone Marengo', hobby: 'Paddle', gender: 'M',isPro:false },
+  //   { name: 'Paolo Vietti', hobby: 'Paddle', gender: 'M',isPro:false },
+  //   { name: 'Federico Mameli', hobby: 'Danza', gender: 'M',isPro:false },
+  //   { name: 'Lorenzo Cravero', hobby: 'Calcio', gender: 'M',isPro:false },
+  //   { name: 'Marta Baudracco', hobby: 'Volley', gender: 'F',isPro:false },
+  //   { name: 'Edoardo Ababei', hobby: 'Palestra', gender: 'M',isPro:false },
+  //   { name: 'Ivan Anjelovski', hobby: 'Palestra', gender: 'M',isPro:false }
+  // ]
+
+  studentList : any[] = [];
+  studentName:string = "";
+
+  onAddStudent(){
+    let newStudent = {name:this.studentName, hobby:'Karate',gender:'M',isPro:true};
+    this.studentList.push(newStudent);
+    this.studentName = "";
+  }
 }
